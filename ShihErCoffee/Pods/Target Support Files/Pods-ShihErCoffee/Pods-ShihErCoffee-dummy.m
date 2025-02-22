cc01551aaa77b1838f59be778033bc20d6aa880f
Hill <angelblue065@gmail.com>
2025-02-22 13:05:36 +0800

    feat: pod install, gitignore, fix: ENABLE_USER_SCRIPT_SANDBOXING = NO;

---
+++ ShihErCoffee/Pods/Target Support Files/Pods-ShihErCoffee/Pods-ShihErCoffee-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShihErCoffee : NSObject
+@end
+@implementation PodsDummy_Pods_ShihErCoffee
+@end
---
+++ ShihErCoffee/ShihErCoffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShihErCoffee
+//
+//  Created by Lin Hill on 2025/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
